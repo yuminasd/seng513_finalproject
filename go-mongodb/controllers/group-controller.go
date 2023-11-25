1495f79b1197c51c4e3b6768b30ce73047885df2
Zainab Syeda <68014006+bi-yotch@users.noreply.github.com>
2023-11-25 16:26:32 -0700

    Updated group model, and defined functions for Group Controller.

--- go-mongodb/controllers/group-controller.go
+++ go-mongodb/controllers/group-controller.go
@@ -7,3 +7,19 @@ import (
 )
 
 var groupCollection *mongo.Collection = configs.GetCollection(configs.DB, "groups")
+
+//Zainab
+//func CreateGroup
+
+//func GetGroupInfo(id) / get all name, users, genre
+
+//func UpdateGroup(id) / group name, genre, code
+
+//func DeleteGroup(id)
+
+//Mahian1
+//func GetLikedMovies(id) / returning movies most liked by users
+
+//func RemoveLikedMovies(Movie.Id)
+
+//func RemoveUser(User.id)
--- go-mongodb/models/group-models.go
+++ go-mongodb/models/group-models.go
@@ -3,8 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Group struct {
-	Id       primitive.ObjectID `json:"id,omitempty"`
-	Name     string             `json:"name,omitempty" validate:"required"`
-	Location string             `json:"location,omitempty" validate:"required"`
-	Title    string             `json:"title,omitempty" validate:"required"`
+	Id       	primitive.ObjectID 	`json:"id,omitempty"`
+	Name     	string             	`json:"name,omitempty" validate:"required"`
+	Code	 	string				`json:"code,omitempty"`
+	Genre 	 	[]string			`json:"genre,omitempty"`
+	Members 	[]User				`json:"User,omitempty"`
+	LikedMovies []string			`json:"likedMovies,omitempty"`
 }
