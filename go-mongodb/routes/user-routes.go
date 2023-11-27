6ed72f04bb1c4a3c53ce3ceb92f2c9d396ffa7bb
Ty-Irving <ty.irving1@ucalgary.ca>
2023-11-27 13:02:38 -0700

    Finished most of user controller and added to routes

--- go-mongodb/routes/user-routes.go
+++ go-mongodb/routes/user-routes.go
@@ -11,4 +11,13 @@ func UserRoute(router *gin.Engine) {
 	router.GET("/users/:userId", controllers.GetUser())
 	router.GET("/users", controllers.GetAllUsers())
 	router.DELETE("users/:userId", controllers.DeleteAUser())
+	router.PUT("/users/:userId", controllers.UpdateUser())
+	router.PATCH("/removegroup/:userId/:groupId", controllers.RemoveGroup())
+	router.PATCH("/addgroup/:userId/:groupId", controllers.AddGroup())
+	router.PATCH("/addliked/:userId/:movieId", controllers.AddLiked())
+	router.PATCH("/removeliked/:userId/:movieId", controllers.RemoveLiked())
+	router.PATCH("/adddisliked/:userId/:movieId", controllers.AddDisliked())
+	router.PATCH("/removedisliked/:userId/:movieId", controllers.RemoveDisliked())
+	router.GET("users/liked/:userId", controllers.GetLiked())
+	router.GET("users/disliked/:userId", controllers.GetDisliked())
 }
