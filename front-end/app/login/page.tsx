ddf69062b78e6a822f629af3382f00b5682e0166
vihari-v <viharivadlamudi@gmail.com>
2023-12-01 14:37:23 -0700

    login page created

--- front-end/app/login/page.tsx
+++ front-end/app/login/page.tsx
@@ -1,8 +1,11 @@
 'use client'
-import { useRouter } from 'next/navigation'
+// Import necessary components
+import { useRouter } from 'next/navigation';
+import Button from '../components/button';
 
 export default function Page() {
     const router = useRouter();
+
     // Your login logic here...
     const handleLogin = () => {
         // Perform login actions...
@@ -10,10 +13,13 @@ export default function Page() {
     };
 
     return (
-        <div>
-            <input placeholder="email" />
-            <input placeholder="password" />
-            <button onClick={handleLogin}>Login</button>
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-64 text-center">
+                <h1 className="text-3xl font-bold mb-4 text-purple-500">Please Sign In</h1>
+                <input className="my-2 p-2 w-full border border-gray-300 rounded text-black" type="text" placeholder="Email" />
+                <input className="my-2 p-2 w-full border border-gray-300 rounded text-black" type="password" placeholder="Password" />
+                <Button text="Login" color="primary" onClick={handleLogin} />
+            </div>
         </div>
     );
-}
+    }
