b8ca3ace6f4bed758edede1ae0ff8f37680f92be
zach-brown <60795970+zach-brown@users.noreply.github.com>
2023-12-01 00:31:04 -0700

    Added functionality to swipe page

---
+++ front-end/app/swipe/page.tsx
@@ -0,0 +1,11 @@
+export default function Swipe() {
+    return (
+        <main>
+            <select className="selectgroupbox" >
+                <option value="" selected disabled hidden>Groups</option>
+                <option value="Group 1">Group 1</option>
+                <option value="Group 2">Group 2</option>   
+            </select>
+        </main>
+    )
+}
