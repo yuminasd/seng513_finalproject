205db7babc274564e3603a3e1cf75865eeec83e1
mahianm <101750087+mahianm@users.noreply.github.com>
2023-12-01 11:23:47 -0700

    Implemented group controller functions

--- go-mongodb/routes/user-routes.go
+++ go-mongodb/routes/user-routes.go
@@ -10,5 +10,15 @@ func UserRoute(router *gin.Engine) {
 	router.POST("/users", controllers.CreateUser())
 	router.GET("/users/:userId", controllers.GetUser())
 	router.GET("/users", controllers.GetAllUsers())
-	router.DELETE("users/:userId", controllers.DeleteAUser())
+
+	router.POST("/groups", controllers.CreateGroup())
+	router.POST("/groups/:groupId/genres", controllers.AddGenresToGroup())
+	router.DELETE("/groups/:groupId/genres", controllers.DeleteGenresFromGroup())
+	router.POST("/groups/:groupId/members", controllers.AddMembersToGroup())
+	router.DELETE("/groups/:groupId/members/:userId", controllers.RemoveUserFromGroup())
+	router.POST("/groups/:groupId/likedmovies", controllers.AddAllUsersLikedMoviesToGroup())
+	router.GET("/groups/:groupId/likedmovies", controllers.GetLikedMoviesFromGroup())
+	router.DELETE("/groups/:groupId/likedmovies/:movieId", controllers.DeleteLikedMovieFromGroup())
+	router.POST("/groups/:groupId/likedmovies/:movieId", controllers.AddMovieToGroupLikedMovies())
+	router.POST("/groups/:groupId/users/:userId/likedmovies", controllers.AddUserLikedMoviesToGroup())
 }
