85d734efe923bea11e2cec551751a0266e490bea
vihari-v <viharivadlamudi@gmail.com>
2023-12-06 15:20:07 -0700

    signup page created, not yet linked to login

--- front-end/app/login/page.tsx
+++ front-end/app/login/page.tsx
@@ -1,111 +1,119 @@
 'use client'
-// login/page.tsx
-
 import { useState } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import Button from '../components/button';
 
+// ...
+
+// ...
+
+// ...
+
 export default function Page() {
-  const router = useRouter();
-  const [showPassword, setShowPassword] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [showError, setShowError] = useState(false);
+    const router = useRouter();
+    const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showError, setShowError] = useState(false); // New state for error pop-up
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/checklogin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email: email, password: password }),
-      });
-
-      if (response.ok) {
+    const handleLogin = async () => {
         try {
-          const responseData = await response.json();
-          console.log(responseData);
+            const response = await fetch('http://localhost:5000/checklogin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "email": email, "password": password }),
+            });
 
-          const userId = responseData.data.id;
-          const userRole = responseData.data.userRole;
+            if (response.ok) {
+                // Handle successful login
+                try {
+                    const responseData = await response.json();
+                    console.log(responseData);
 
-          localStorage.setItem('userId', userId);
-          localStorage.setItem('userRole', userRole);
+                    const userId = responseData.data.id;
+                    const userRole = responseData.data.userRole;
 
-          router.push(`/?userid=${userId}`);
+                    localStorage.setItem('userId', userId);
+                    localStorage.setItem('userRole', userRole);
+
+                    router.push(`/?userid=${userId}`);
+                } catch (error) {
+                    console.error('Error parsing JSON in the API response:', error);
+                }
+            } else {
+                // Handle login failure
+                console.error('Login failed');
+                setShowError(true); // Show the error pop-up
+            }
         } catch (error) {
-          console.error('Error parsing JSON in the API response:', error);
+            // Handle fetch error
+            console.error('Error during login:', error);
         }
-      } else {
-        console.error('Login failed');
-        setShowError(true);
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-    }
-  };
+    };
 
-  const storedUserId = localStorage.getItem('userId');
+    // Retrieve user id from localStorage
+    const storedUserId = localStorage.getItem('userId');
 
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <div className="w-120 text-center">
-        <div className="mb-4">
-          <img src="/logo.png" alt="logo" className="mx-auto" />
-        </div>
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+            <div className="w-120 text-center">
+                {/* MovieMatch Logo */}
+                <div className="mb-4">
+                    <img src="/logo.png" alt="logo" className="mx-auto" />
+                </div>
 
-        <h1 className="text-3xl font-bold mb-4">
-          <span className="text-white">Welcome To</span>
-          <span className="text-purple-500"> Movie Match</span>
-        </h1>
-        <div className="mb-4 w-120">
-          <label className="text-white block mb-1 text-left">Email</label>
-          <input
-            className="my-2 p-2 w-full border rounded text-gray-500 bg-gray-800 border-transparent"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
+                <h1 className="text-3xl font-bold mb-4">
+                    <span className="text-white">Welcome To</span>
+                    <span className="text-purple-500"> Movie Match</span>
+                </h1>
+                <div className="mb-4 w-120">
+                    <label className="text-white block mb-1 text-left">Email</label>
+                    <input
+                        className="my-2 p-2 w-full border rounded text-gray-500 bg-gray-800 border-transparent"
+                        type="text"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
 
-        <div className="mb-4 w-120 relative">
-          <label className="text-white block mb-1 text-left">Password</label>
-          <div className="flex items-center">
-            <input
-              className="my-2 p-2 w-full border rounded text-gray-500 bg-gray-800 border-transparent"
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <span
-              className="text-gray-400 text-sm cursor-pointer ml-2 bg-gray-800 p-2 rounded"
-              onClick={() => setShowPassword(!showPassword)}
-            >
-              {showPassword ? '👁️' : '👁️'}
-            </span>
-          </div>
-        </div>
+                <div className="mb-4 w-120 relative">
+                    <label className="text-white block mb-1 text-left">Password</label>
+                    <div className="flex items-center">
+                        <input
+                            className="my-2 p-2 w-full border rounded text-gray-500 bg-gray-800 border-transparent"
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <span
+                            className="text-gray-400 text-sm cursor-pointer ml-2 bg-gray-800 p-2 rounded"
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                            {showPassword ? '👁️' : '👁️'}
+                        </span>
+                    </div>
+                </div>
 
-        <Button text="Sign In" color="primary" onClick={handleLogin} />
-        <Link href="/app/signup/page">
-          <a className="mt-2 text-gray-400 text-sm cursor-pointer">Create Account</a>
-        </Link>
+                <Button text="Sign In" color="primary" onClick={handleLogin} />
+                <div className="mt-2 text-gray-400 text-sm cursor-pointer">Create Account</div>
 
-        {showError && (
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
-            <div className="bg-gray-800 p-4 rounded shadow-md text-white">
-              <p className="text-red-500">Invalid email or password. Please try again.</p>
-              <button className="mt-2 p-2 bg-purple-500 text-white rounded" onClick={() => setShowError(false)}>
-                Close
-              </button>
+                {/* Error pop-up */}
+                {showError && (
+                    <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+                        <div className="bg-gray-800 p-4 rounded shadow-md text-white">
+                            <p className="text-red-500">Invalid email or password. Please try again.</p>
+                            <button className="mt-2 p-2 bg-purple-500 text-white rounded"
+                                onClick={() => setShowError(false)}>
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
--- front-end/app/signup/page.tsx
+++ front-end/app/signup/page.tsx
@@ -1,76 +1,109 @@
 'use client'
-
-// app/signup/page.tsx
-
+// Import necessary dependencies
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import Button from '../components/button';
 
-export default function SignUpPage() {
-  const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+// Create and export the Signup component
+export default function Signup() {
+    // Initialize necessary state variables
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [showError, setShowError] = useState(false);
 
-  const handleSignUp = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          // Add any additional fields needed for user creation
-        }),
-      });
+    // Function to handle signup
+    const handleSignup = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ "email": email, "password": password }),
+            });
 
-      if (response.ok) {
-        // Redirect to the login page after successful signup
-        router.push('/login/page');
-      } else {
-        console.error('Account creation failed');
-      }
-    } catch (error) {
-      console.error('Error during account creation:', error);
-    }
-  };
+            if (response.ok) {
+                // Handle successful signup
+                router.push('/login'); // Redirect to the login page
+            } else {
+                // Handle signup failure
+                console.error('Signup failed');
+                setShowError(true);
+            }
+        } catch (error) {
+            // Handle fetch error
+            console.error('Error during signup:', error);
+        }
+    };
 
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-      <div className="w-120 text-center">
-        <h1 className="text-3xl font-bold mb-4">
-          <span className="text-white">Create Your</span>
-          <span className="text-purple-500"> Account</span>
-        </h1>
-        <div className="mb-4 w-120">
-          <label className="text-white block mb-1 text-left">Email</label>
-          <input
-            className="my-2 p-2 w-full border rounded text-gray-500 bg-gray-800 border-transparent"
-            type="text"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
+            <div className="w-120 text-center">
+                {/* Same logo as the login page */}
+                <div className="mb-4">
+                    <img src="/logo.png" alt="logo" className="mx-auto" />
+                </div>
+
+                <h1 className="text-3xl font-bold mb-4">
+                    <span className="text-white">Create Your</span>
+                    <span className="text-purple-500"> Movie Match Account</span>
+                </h1>
+                
+                {/* Email input */}
+                <div className="mb-4 w-120">
+                    <label className="text-white block mb-1 text-left">Email</label>
+                    <input
+                        className="my-2 p-2 w-full border rounded text-gray-500 bg-gray-800 border-transparent"
+                        type="text"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                {/* Password input with show/hide toggle */}
+                <div className="mb-4 w-120 relative">
+                    <label className="text-white block mb-1 text-left">Password</label>
+                    <div className="flex items-center">
+                        <input
+                            className="my-2 p-2 w-full border rounded text-gray-500 bg-gray-800 border-transparent"
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <span
+                            className="text-gray-400 text-sm cursor-pointer ml-2 bg-gray-800 p-2 rounded"
+                            onClick={() => setShowPassword(!showPassword)}
+                        >
+                            {showPassword ? '👁️' : '👁️'}
+                        </span>
+                    </div>
+                </div>
+
+                {/* Signup button */}
+                <Button text="Sign Up" color="primary" onClick={handleSignup} />
+
+                {/* Link to login page */}
+                <div className="mt-2 text-gray-400 text-sm cursor-pointer" onClick={() => router.push('/login')}>
+                    Already have an account? Login here.
+                </div>
+
+                {/* Error pop-up */}
+                {showError && (
+                    <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+                        <div className="bg-gray-800 p-4 rounded shadow-md text-white">
+                            <p className="text-red-500">Signup failed.</p>
+                            <button className="mt-2 p-2 bg-purple-500 text-white rounded"
+                                onClick={() => setShowError(false)}>
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
-
-        <div className="mb-4 w-120 relative">
-          <label className="text-white block mb-1 text-left">Password</label>
-          <input
-            className="my-2 p-2 w-full border rounded text-gray-500 bg-gray-800 border-transparent"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-
-        <button
-          className="my-2 p-2 w-full bg-purple-500 text-white rounded cursor-pointer"
-          onClick={handleSignUp}
-        >
-          Sign Up
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
